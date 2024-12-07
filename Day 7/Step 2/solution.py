73b8dddb0950d8c278d786655d99a0043c9417b4
Giovanni Cinel <96290564+GiovanniCinel@users.noreply.github.com>
2024-12-07 19:13:35 +0100

    Update solution.py

--- Day 7/Step 2/solution.py
+++ Day 7/Step 2/solution.py
@@ -1,20 +1,3 @@
-def findOperation(result, vec, partial):
-    if partial > result:
-        False
-    if(len(vec) == 1):
-        if(result == partial * vec[0]):
-            return True
-        if(result == partial + vec[0]):
-            return True
-        return False
-    
-    mulWay = partial * vec[0]
-    sumWay = partial + vec[0]
-
-    if(findOperation(result, vec[1:], mulWay) or findOperation(result, vec[1:], sumWay)):
-        return True
-    return False
-
 def findOperationWithConcatenation(result, vec, partial):
     if(len(vec) == 1):
         if(result == partial * vec[0]):
@@ -48,7 +31,6 @@ input.close()
 
 for i in range(0, len(data)):
     result = data[i][0]
-    print("processing ", i)
     if(findOperationWithConcatenation(result, data[i][2:], data[i][1])):
         total += result
 
