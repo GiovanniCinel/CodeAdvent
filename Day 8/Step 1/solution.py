f1dce337dfc197b4aff83d0a44a21c29f976ef8e
Giovanni Cinel <giovannicinel9@gmail.com>
2024-12-08 16:04:11 +0100

    Day 8 Step 1

---
+++ Day 8/Step 1/solution.py
@@ -0,0 +1,57 @@
+def printMap(map):
+    for i in range(0, len(map)):
+        str = ""
+        for j in range(0, len(map[i])):
+            str += map[i][j]
+        print(str)
+
+from collections import defaultdict
+
+input = open("input.txt", "r")
+line = input.readline()
+
+map = defaultdict(list)
+antinodeMap = []
+total = 0
+
+row = 0
+while(line != ""):
+    column = 0
+    antinodeMap.append([x for x in line if x != '\n'])
+    for char in line:
+        if(char not in map.keys()):
+            map[char] = [[row, column]]
+        else:
+            map[char].append([row, column])
+        column += 1
+    line = input.readline()
+    row += 1
+
+input.close()
+
+map.pop('.')
+map.pop('\n')
+
+for key in map.keys():
+    for i in range(0, len(map[key])):
+        for j in range(0, len(map[key])):
+            if(j != i):
+                rowI = map[key][i][0]
+                colI = map[key][i][1]
+                rowJ = map[key][j][0]
+                colJ = map[key][j][1]
+                deltaRow = rowJ - rowI
+                deltaCol = colJ - colI
+                antennaRow = rowJ + deltaRow
+                antennaCol = colJ + deltaCol
+                if((antennaRow in range(0, len(antinodeMap))) and (antennaCol in range(0, len(antinodeMap[antennaRow])))):
+                    antinodeMap[antennaRow][antennaCol] = '#'
+
+printMap(antinodeMap)
+
+for i in range(0, len(antinodeMap)):
+        for j in range(0, len(antinodeMap[i])):
+            if(antinodeMap[i][j] == '#'):
+                total += 1
+
+print("The number of distinct antinodes is : ", total)
